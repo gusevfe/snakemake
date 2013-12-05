1642ad31caa24c3def37c01b14440f658b6da81b
Gusev Fedor <gusevfe@gmail.com>
2013-12-06 02:06:42 +0300

    Fix issues.

--- snakemake/jobscript.sh
+++ snakemake/jobscript.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+#$ -e {workdir}
+#$ -S /bin/bash
 # properties = {properties}
-{workflow.snakemakepath} --snakefile {workflow.snakefile} \
+source /data/results/gusev/software/exports
+
+snakemake --snakefile {workflow.snakefile} \
 --force -j{cores} \
 --directory {workdir} --nocolor --notemp --quiet --nolock {job.output} \
 && touch "{jobfinished}" || touch "{jobfailed}"
